8ade0601b2f85441df2fa3bd556cfb157772fc72
marcoNaik <marcogomezf@gmail.com>
2025-12-23 15:47:20 +1100

    feat(native/migrations): add migration for field template display fields

---
+++ apps/native/src/db/migrations/0002_colorful_greymalkin.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `field_templates` ADD `subheader` text;--> statement-breakpoint
+ALTER TABLE `field_templates` ADD `display_style` text;
--- apps/native/src/db/migrations/migrations.js
+++ apps/native/src/db/migrations/migrations.js
@@ -3,12 +3,14 @@
 import journal from './meta/_journal.json';
 import m0000 from './0000_polite_lightspeed.sql';
 import m0001 from './0001_left_selene.sql';
+import m0002 from './0002_colorful_greymalkin.sql';
 
   export default {
     journal,
     migrations: {
       m0000,
-m0001
+m0001,
+m0002
     }
   }
   
