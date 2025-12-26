cd269f84153317a78b28d4c23253e30074889505
marcoNaik <marcogomezf@gmail.com>
2025-12-26 23:06:35 +1100

    refactor(web/form-builder): simplify droppable zone to minimal implementation

--- apps/web/src/components/form-builder/DroppableZone.tsx
+++ apps/web/src/components/form-builder/DroppableZone.tsx
@@ -4,20 +4,9 @@ import { useDroppable } from "@dnd-kit/core";
 
 interface DroppableZoneProps {
   id: string;
-  isOver: boolean;
 }
 
-export function DroppableZone({ id, isOver }: DroppableZoneProps) {
+export function DroppableZone({ id }: DroppableZoneProps) {
   const { setNodeRef } = useDroppable({ id });
-
-  return (
-    <div
-      ref={setNodeRef}
-      className={`transition-all rounded ${
-        isOver
-          ? "h-16 bg-blue-50 border-2 border-dashed border-blue-400 my-2"
-          : "h-1 my-0"
-      }`}
-    />
-  );
+  return <div id={id} ref={setNodeRef} className="h-1" />;
 }
