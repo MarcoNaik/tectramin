ac26589ebb8a542dd34e86a6380e8e31d19a6a05
marcoNaik <marcogomezf@gmail.com>
2026-01-08 15:43:37 +1100

    refactor(native/components/fields): export expanded field context and entity select field

--- apps/native/src/components/fields/index.ts
+++ apps/native/src/components/fields/index.ts
@@ -1,3 +1,5 @@
 export { FieldInput } from "./FieldInput";
 export { SelectField } from "./SelectField";
 export { UserSelectField } from "./UserSelectField";
+export { EntitySelectField } from "./EntitySelectField";
+export { ExpandedFieldProvider, useExpandedField, useCollapseAll } from "./ExpandedFieldContext";
