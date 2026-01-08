7e4c2f453dac580566fc5afd97ea5982bf6ffaa7
marcoNaik <marcogomezf@gmail.com>
2026-01-08 15:43:37 +1100

    feat(native/db/schema): add readme column to day task templates table

--- apps/native/src/db/schema.ts
+++ apps/native/src/db/schema.ts
@@ -19,6 +19,7 @@ export const dayTaskTemplates = sqliteTable("day_task_templates", {
   taskTemplateServerId: text("task_template_server_id").notNull(),
   taskTemplateName: text("task_template_name").notNull(),
   description: text("description"),
+  readme: text("readme"),
   order: integer("order").notNull(),
   isRequired: integer("is_required", { mode: "boolean" }).notNull(),
   isRepeatable: integer("is_repeatable", { mode: "boolean" }).notNull().default(false),
