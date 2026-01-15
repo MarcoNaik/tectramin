94959e457c72989bb307dca816abc2d810aece68
marcoNaik <marcogomezf@gmail.com>
2026-01-16 08:01:00 +1100

    feat(web/types): add documentBuilder to main view types

--- apps/web/src/types/index.ts
+++ apps/web/src/types/index.ts
@@ -64,7 +64,7 @@ export interface TaskTemplateData {
   updatedAt: number;
 }
 
-export type MainView = "gridView" | "formBuilder" | "debug" | "dataVisualization";
+export type MainView = "gridView" | "formBuilder" | "debug" | "dataVisualization" | "documentBuilder";
 
 export type DatePreset = "today" | "thisWeek" | "thisMonth" | "allTime";
 
